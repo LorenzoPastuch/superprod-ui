d969cba41ad20bda57f653f653f6b9a9ae8dbf7a
supercopoadm <injexprodadm@gmail.com>
2024-10-25 15:22:04 -0300

    versao 2.0

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    whiteListedDomains: ['192.168.90.80'],
-    blackListedDomains: ['https://192.168.90.80/oauth/token'],
-    apiUrl: 'https://192.168.90.80',
+    whiteListedDomains: ['192.168.90.80:8000'],
+    blackListedDomains: ['https://192.168.90.80:8000/oauth/token'],
+    apiUrl: 'https://192.168.90.80:8000',
     version: '1.0.0'
 }

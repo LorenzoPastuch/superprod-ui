e9f3df47461451ce84b8fc3195aa31a679a5174e
unknown <lorenzopastuch@gmail.com>
2024-11-29 08:28:34 -0300

    websocket

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    whiteListedDomains: ['192.168.90.80:8000'],
-    blackListedDomains: ['http://192.168.90.80:8000/oauth/token'],
-    apiUrl: 'http://192.168.90.80:8000',
+    whiteListedDomains: ['192.168.90.48:8000'],
+    blackListedDomains: ['http://192.168.90.48:8000/oauth/token'],
+    apiUrl: 'http://192.168.90.48:8000',
     version: '1.0.0'
 }

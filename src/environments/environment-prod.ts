275a4457a0753a19f36ed1741b819c04dd35e9a4
unknown <lorenzopastuch@gmail.com>
2025-06-23 16:06:32 -0300

    correçao IP

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    whiteListedDomains: ['192.168.90.59:8000'],
-    blackListedDomains: ['http://192.168.90.59:8000/oauth/token'],
-    apiUrl: 'http://192.168.90.59:8000',
+    whiteListedDomains: ['192.168.87.59:8000'],
+    blackListedDomains: ['http://192.168.87.59:8000/oauth/token'],
+    apiUrl: 'http://192.168.87.59:8000',
     version: '1.0.0',
-    websocket: 'ws://192.168.90.59:8000'
+    websocket: 'ws://192.168.87.59:8000'
 }

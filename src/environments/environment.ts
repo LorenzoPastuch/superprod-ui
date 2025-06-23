b834d88912be3c70dd11f5292861d7235930f197
unknown <lorenzopastuch@gmail.com>
2025-06-23 16:14:33 -0300

    correçao IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    whiteListedDomains: ['192.168.87.59:8000'],
-    blackListedDomains: ['http://192.168.87.59:8000/oauth/token'],
-    apiUrl: 'http://192.168.87.59:8000',
+    whiteListedDomains: ['localhost:8000'],
+    blackListedDomains: ['http://localhost:8000'],
+    apiUrl: 'http://localhost:8000',
     version: '1.0.0',
-    websocket: 'ws://192.168.87.59:8000'
+    websocket: 'ws://localhost:8000'
 }
